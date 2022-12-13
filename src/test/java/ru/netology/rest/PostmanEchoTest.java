cd0daf11b739bb2828a3bd235dae7befa391a2c5
Victor Petrunin <petruninviktor9464@gmail.com>
2022-12-13 17:07:54 +0300

    first commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Data"))
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+
+    }
+
+
+}
